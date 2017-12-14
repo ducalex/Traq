4d61cba722f9f02bbae635623462be0736d791fd
Alex Duchesne <alex@netvps.ca>
2017-12-14 17:50:14 -0500

    Prism.js plugin for code highlighting

---
+++ vendor/traq/plugins/prism/prism.php
@@ -0,0 +1,36 @@
+<?php
+namespace traq\plugins;
+
+use avalon\http\Router;
+use avalon\http\Request;
+use avalon\Autoloader;
+use \FishHook;
+use \HTML;
+
+class Prism extends \traq\libraries\Plugin
+{
+    protected static $info = array(
+        'name'    => 'Prism.js',
+        'version' => '1.0',
+        'author'  => 'alex'
+    );
+
+    /**
+     * Handles the startup of the plugin.
+     */
+    public static function init()
+    {
+        FishHook::add('template:layouts/global/head', array(get_called_class(), 'insert'));
+    }
+
+    public static function insert()
+    {
+		static $inserted = false;
+
+		if ($inserted == false) {
+            echo HTML::js_inc(Request::base('/js.php?plugin=prism&js=all'));
+            echo HTML::css_link(Request::base('/css.php?plugin=prism&css=all'));
+			$inserted = true;
+		}
+    }
+}
